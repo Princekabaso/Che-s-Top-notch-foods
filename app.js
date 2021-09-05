0145ad46ac40b6fd1748f95e45bedb98f7404cd6
Prince Kabaso <68119982+Princekabaso@users.noreply.github.com>
2021-09-05 19:07:42 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+
+menubtn.addEventListener("click", function(){
+    navbarM.style.animation = "nav .3s linear forwards";
+    body.style.animation = "sc .1s linear forwards";
+})
+cancel.addEventListener("click", function(){
+    navbarM.style.animation = "";
+    body.style.animation = "";
+})
+off.addEventListener("click", function(){
+    navbarM.style.animation = "";
+    body.style.animation = "";
+})
+val.addEventListener("click", function(){
+    navbarM.style.animation = "";
+    body.style.animation = "";
+}) 
+serv.addEventListener("click", function(){
+    navbarM.style.animation = "";
+    body.style.animation = "";
+})
+cont.addEventListener("click", function(){
+    navbarM.style.animation = "";
+    body.style.animation = "";
+})
